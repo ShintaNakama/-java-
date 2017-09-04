b1767f0d407b4ee54de0017bb8c7445362eec502
nakamashinta <nakamashinta@192.168.11.8>
2017-09-05 08:36:16 +0900

    いやー

---
+++ eclipse-workspace/docoTsubu/src/model/LoginLogic.java
@@ -0,0 +1,15 @@
+/**
+ * 
+ */
+package model;
+
+/**
+ * @author nakamashinta
+ *
+ */
+public class LoginLogic {
+  public boolean execute(User user ) {
+	if(user.getPass().equals("1234")) { return true; }
+	return false;
+  }
+}
